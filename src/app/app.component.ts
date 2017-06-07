4a6148d735f65fe0a5b52160996d62d694d22748
pcboby <pcboby@126.com>
2017-06-07 08:40:50 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private translate: TranslateService) {
+        translate.addLangs(['en', 'cn']);
+        translate.setDefaultLang('en');
+
+        const browserLang = translate.getBrowserLang();
+        translate.use(browserLang.match(/en|cn/) ? browserLang : 'en');
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Http, HttpModule } from '@angular/http';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routes';
+
+export function HttpLoaderFactory(http:Http) {
+  return new TranslateHttpLoader(http, '/assets/i18n/', '.json')
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +17,15 @@ import { AppRoutes } from './app.routes';
   ],
   imports: [
     BrowserModule,
-    AppRoutes
+    AppRoutes,
+    HttpModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [Http]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
