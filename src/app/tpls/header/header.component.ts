07f2311ae515796fc212c2cbd300675406ef7292
pcboby <pcboby@126.com>
2017-06-08 10:52:59 +0800

    update

--- src/app/tpls/header/header.component.ts
+++ src/app/tpls/header/header.component.ts
@@ -14,9 +14,10 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
   }
 
-  changeLang(lang: string){
+  changeLang(lang: string) {
     console.log(lang)
     this.translate.use(lang)
+    localStorage.lang = lang;
   }
 
 }
